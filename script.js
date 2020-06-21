957c57cef4cdf666f162f420ce71c14d428e2fea
Jaime C Smith <computerprogrammer0414@gmail.com>
2020-06-20 21:32:30 -0400

    Continue in adding the questions via JS

--- script.js
+++ script.js
@@ -1,54 +1,117 @@
 
-/*********************************
- *********QUIZ CONTROLLER*********
-******************************** */
+/*******************************
+*********QUIZ CONTROLLER********
+*******************************/
 var quizController = (function() {
 
-   //*******Question Constructor */
-   function Question(id, questionText, options, correctAnswer){
+   //*********Question Constructor*********/
+  function Question(id, questionText, options, correctAnswer) {
       this.id = id;
       this.questionText = questionText;
       this.options = options;
       this.correctAnswer = correctAnswer;
-   }
+  }
 
-    return {
-        addQuestiononLocalStorage: function(newQuestText, opts){
-          console.log('Hi');
-        }
-    }
+   var questionLocalStorage = {
+      // 35
+      setQuestionCollection: function(newCollection) {
+          localStorage.setItem('questionCollection', JSON.stringify(newCollection));
+      },
+        getQuestionCollection: function() {
+          return JSON.parse(localStorage.getItem('questionCollection'));
+      }, 
+         removeQuestionCollection: function() {
+          localStorage.removeItem('questionCollection');
+      }
+  }
+
+   return {
+     
+      addQuestionOnLocalStorage: function(newQuestText, opts) {
+         
+          var optionsArr, corrAns, questionId, newQuestion, getStoredQuests;
+          
+          if(questionLocalStorage.getQuestionCollection() === null) {
+              questionLocalStorage.setQuestionCollection([]);
+          }
+          
+          optionsArr = [];
+         
+          for(var i = 0; i < opts.length; i++) {
+            
+              if(opts[i].value !== "") {
+                  
+                  optionsArr.push(opts[i].value);
+              }
+             
+              if(opts[i].previousElementSibling.checked && opts[i].value !== "") {
+                  
+                  corrAns = opts[i].value;
+              }
+          }
+
+          if(questionLocalStorage.getQuestionCollection().length > 0) {
+             
+              questionId = questionLocalStorage.getQuestionCollection()[questionLocalStorage.getQuestionCollection().length - 1].id + 1;
+              
+          } else {
+              questionId = 0;
+          }
+
+                  newQuestion = new Question(questionId, newQuestText.value, optionsArr, corrAns);
+         
+          getStoredQuests = questionLocalStorage.getQuestionCollection();
+        
+          getStoredQuests.push(newQuestion);
+          
+          questionLocalStorage.setQuestionCollection(getStoredQuests);
+        
+          console.log(questionLocalStorage.getQuestionCollection());
+      }
+  };
 
 })();
 
-/*********************************
- *********UICONTROLLER*********
-******************************** */
-var UIController = (function(){
+/*******************************
+**********UI CONTROLLER*********
+*******************************/
 
-    var domItems = {
-      //***********Admin Panel */
-        questInsertBtn: document.getElementById("question-insert-btn"),
-        newQuestionText: document.getElementById("new-question-text"),
-        adminOptions: document.querySelectorAll(".admin-option")
-    };
+var UIController = (function() {
 
-    return {
-        getDomItems: domItems
-    };
+ 
+  var domItems = {
+      //*******Admin Panel Elements********/
+      questInsertBtn: document.getElementById('question-insert-btn'), // 6
+      newQuestionText: document.getElementById('new-question-text'), // 15
+      adminOptions: document.querySelectorAll('.admin-option') // 16
+  };
+
+ 
+  return {
+      getDomItems: domItems 
+  };
 
 })();
 
-/*********************************
- *********CONTROLLER*********
-******************************** */
+/*******************************
+***********CONTROLLER***********
+*******************************/
+
 var controller = (function(quizCtrl, UICtrl) {
 
+ 
   var selectedDomItems = UICtrl.getDomItems;
 
-   selectedDomItems.questInsertBtn.addEventListener('click', function(){
+  
+  selectedDomItems.questInsertBtn.addEventListener('click', function() {
 
-    quizCtrl.addQuestiononLocalStorage(selectedDomItems.newQuestionText, selectedDomItems.adminOptions);
-    
-   });
+      quizCtrl.addQuestionOnLocalStorage(selectedDomItems.newQuestionText, selectedDomItems.adminOptions);
+
+  });
 
 })(quizController, UIController);
+
+
+
+
+
